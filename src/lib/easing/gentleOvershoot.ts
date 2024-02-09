904fc9c4a8cdf3ea3fa2e3c6806976f9fb556220
Jaden Rose <jaden.rose@criticalmass.com>
2024-02-09 09:21:48 -0700

    fix: easing folder structure

---
+++ src/lib/easing/gentleOvershoot.ts
@@ -0,0 +1,5 @@
+import { bezier } from '@utils'
+
+const gentleOvershoot = bezier(0.31, 0.17, 0.12, 1.23)
+
+export default gentleOvershoot
--- src/lib/easing/index.ts
+++ src/lib/easing/index.ts
@@ -1,3 +1,1 @@
-import { bezier } from '@utils'
-
-export const gentleOvershoot = bezier(0.31, 0.17, 0.12, 1.23)
+export { default as gentleOvershoot } from './gentleOvershoot'
